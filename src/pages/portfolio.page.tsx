e08b1576356658952af4c84849312f2c68a4708f
Piotr Sokołowski <71117424+piosso7@users.noreply.github.com>
2022-09-07 17:54:17 +0200

    Initialization commit and portfolio page header

--- src/pages/portfolio.page.tsx
+++ src/pages/portfolio.page.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './home.page.scss';
+import './portfolio.page.scss';
+import headerImg from '../img/portfolio-header.png';
+import logo from '../img/logo.png';
 
 const PortfolioPage = () => {
-    return <div>Start your portfolio here</div>;
+    return (
+        <div className="portfolio">
+            <div className="portfolio-header">
+                <img className="portfolio-header-img" src={headerImg} alt="header img" />
+                <img className="portfolio-header-logo" src={logo} alt="logo" />
+                <div className="portfolio-header-text">
+                    <h1 className="portfolio-title">With Creative from Heart</h1>
+                    <p>
+                        I want to welcome you to my website, where ahead you will find a fascinating
+                        journey into my world in the world of web design.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PortfolioPage;

6f90e426f8d7b0b9bb9f54456ab4d88761d727dd
Piotr Sokołowski <71117424+piosso7@users.noreply.github.com>
2022-09-09 22:14:36 +0200

    table page: render a list from a JSON file

--- src/pages/table.page.tsx
+++ src/pages/table.page.tsx
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './home.page.scss';
 
-const TablePage = () => {
-    return <div>Start your table here</div>;
-};
+interface IProduct {
+    id: number;
+    product_name: string;
+    price: string;
+    discontinued: string;
+    units: string;
+}
+
+function TablePage() {
+    const [products, setProducts] = useState<IProduct[]>([]);
+
+    //Render a list from a JSON file function
+    function getProduscts() {
+        fetch('http://localhost:3000/data').then((result) => {
+            result.json().then((resp) => {
+                setProducts(resp);
+            });
+        });
+    }
+    useEffect(() => {
+        getProduscts();
+    }, []);
+
+    return (
+        <div>
+            <tbody className="tbody">
+                <button className="addBtn">Add New</button>
+                <tr className="descriptions">
+                    <th className="pName">Product Name</th>
+                    <th>Price</th>
+                    <th>Discontinued</th>
+                    <th>Units In Stock</th>
+                    <th>Command</th>
+                </tr>
+                {products.map((product, i) => (
+                    <tr className="product" key={i}>
+                        <td>{product.product_name}</td>
+                        <td>$ {product.price}</td>
+                        <td>{product.discontinued + ''}</td>
+                        <td>{product.units}</td>
+                        <td>
+                            <button className="editBtn">Edit</button>
+                            <button className="deleteBtn">Remove</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </div>
+    );
+}
 
 export default TablePage;

d70c382f5f8ab26b2f732d5154b045d9812281cc
Piotr Sokołowski <71117424+piosso7@users.noreply.github.com>
2022-09-10 09:41:04 +0200

    table page: del elements from list and del modal

--- src/pages/table.page.tsx
+++ src/pages/table.page.tsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import './home.page.scss';
+import axios from 'axios';
+import Modal from 'react-modal';
 
 interface IProduct {
-    id: number;
+    id: string;
     product_name: string;
     price: string;
     discontinued: string;
@@ -11,6 +13,8 @@ interface IProduct {
 
 function TablePage() {
     const [products, setProducts] = useState<IProduct[]>([]);
+
+    const [productId, setId] = useState('');
 
     //Render a list from a JSON file function
     function getProduscts() {
@@ -24,6 +28,53 @@ function TablePage() {
         getProduscts();
     }, []);
 
+    //DELETE FUNCTION
+
+    function deleteProduct(id: string) {
+        fetch(`http://localhost:3000/data/${id}`, {
+            method: 'DELETE',
+        }).then((result) => {
+            result.json().then((resp) => {
+                console.warn(resp);
+                getProduscts();
+                setDeleteIsOpen(false);
+            });
+        });
+    }
+
+    //MODALS
+    // STYLE
+    const productModalStyle = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            fontSize: '24px',
+        },
+    };
+
+    // DELETE PRODUCT MODAL
+
+    const [deleteModalIsOpen, setDeleteIsOpen] = React.useState(false);
+
+    function openDeleteModal(id: string) {
+        setDeleteIsOpen(true);
+        axios
+            .get(`http://localhost:3000/data/${id}`)
+            .then(function () {
+                setId(id);
+            })
+            .catch(function (error: string) {
+                console.log(error);
+            });
+    }
+
+    function closeDeleteModal() {
+        setDeleteIsOpen(false);
+    }
     return (
         <div>
             <tbody className="tbody">
@@ -43,8 +94,23 @@ function TablePage() {
                         <td>{product.units}</td>
                         <td>
                             <button className="editBtn">Edit</button>
-                            <button className="deleteBtn">Remove</button>
+                            <button
+                                className="deleteBtn"
+                                onClick={() => openDeleteModal(product.id)}>
+                                Remove
+                            </button>
                         </td>
+                        {/* DELETE MODAL */}
+
+                        <Modal
+                            ariaHideApp={false}
+                            isOpen={deleteModalIsOpen}
+                            onRequestClose={closeDeleteModal}
+                            style={productModalStyle}>
+                            <h2>Are you sure you want to delete?</h2>
+                            <button onClick={() => deleteProduct(productId)}>Yes</button>
+                            <button onClick={closeDeleteModal}>No</button>
+                        </Modal>
                     </tr>
                 ))}
             </tbody>

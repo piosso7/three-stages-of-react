102f6ab6224a04ed2317f76cf89cebac0d89a18a
Piotr Sokołowski <71117424+piosso7@users.noreply.github.com>
2022-09-19 20:34:15 +0200

    routing task: update reducers index

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,12 +1,24 @@
+import {StickyNoteState} from '@shared/interfaces/stickynote.interface';
 import {combineReducers} from 'redux';
 import CoreReducer, {ICoreState} from './core.reducer';
+import asyncSessionStorage from 'redux-persist/lib/storage';
+import {persistReducer} from 'redux-persist';
+import stickynoteReducer from './stickynote.reducer';
 
 export interface IAppState {
     core: ICoreState;
+    note: StickyNoteState;
 }
+
+const stickyNoteConfig = {
+    key: 'note',
+    storage: asyncSessionStorage,
+    whitelist: ['note'],
+};
 
 const reducers = combineReducers({
     core: CoreReducer,
+    note: persistReducer(stickyNoteConfig, stickynoteReducer),
 });
 
 export default reducers;
